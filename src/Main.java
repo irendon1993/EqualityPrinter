68d1ffa786a4955d3d1c5283255fd78afd22d379
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-01 13:15:48 -0500

    Initial commit / Completed Exercise

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        EqualityPrinter.printEqual(0,-3,7);
+        EqualityPrinter.printEqual(1,2,1);
+        EqualityPrinter.printEqual(3,3,7);
+        EqualityPrinter.printEqual(7,7,7);
+    }
+}
